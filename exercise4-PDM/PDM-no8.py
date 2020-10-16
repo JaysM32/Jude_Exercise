70b5d091117ce56c51c25a5d5d16cd2b101b77d6
JaysM32 <71362967+JaysM32@users.noreply.github.com>
2020-10-16 12:21:16 +0700

    Delete PDM-no8.py

--- exercise4-PDM/PDM-no8.py
+++
@@ -1,9 +0,0 @@
-def longest(list):
-    number = []
-    for i in list:
-        number.append(len(i))
-    return max(number)
-
-
-words = list(input("Enter a list of long words: ").split(","))
-print("Longest word length is ", longest(words))
